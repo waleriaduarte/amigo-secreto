7d60ffb400e62952987ccad6e1c4b983e51fe8ec
waleriaduarte <waleriaduarteuni@gmail.com>
2025-02-20 21:05:16 -0300

    Tarefa 1: Capturar o valor do campo de entrada, no meu caso utilizando o document.getElementById para obter o texto inserido pelo usuário

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// captura o campo de texto e o botão
+const inputNome = document.getElementById('inputAmigo');
+let nomes = []; //armazena os nomes
+
+//função para adicionar nomes
+function adicionarNome() {
+    const nome = inputNome.value.trim();
+    if (nome == '') {
+        alert('Por favor, insira um nome');
+        return;
+    }
+
+nomes.push(nome); // adiciona nomes a lista
+inputNome.value = ''; // limpa o campo de texto
+atualizarLista(); // atualiza a lista na tela
+}
+
+//função para atualizar a lista
+function atualizarLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; // limpa a lista antes de adicionar novos nomes
+    nomes.forEach(nome => {
+        const li= document.createElement('li');
+        li.textContent = nome;
+        listaAmigos.appendChild(li); //adiciona cada nome como um item  de lista
+    });
+}
+
+//captura o botão de adicionar e adiciona o evento "clique"
+const botaoAdicionar = document.getElementById('botaoAdicionar');
+botaoAdicionar.addEventListener('click', adicionarNome);
+
